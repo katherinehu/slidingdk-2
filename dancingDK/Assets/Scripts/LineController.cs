15a3064a8a54c081e92faf6d98920c13cb0fa688
Imani Finkley <ief4@cornell.edu>
2023-05-04 13:14:25 +0200

    line grow

--- dancingDK/Assets/Scripts/LineController.cs
+++ dancingDK/Assets/Scripts/LineController.cs
@@ -6,7 +6,6 @@ public class LineController : MonoBehaviour
 {
     public float speed;
     private Vector3 dir;
-    List<Transform> growth = new List<Transform>();
     public GameObject shadowPrefab;
     private AudioSource backgroundMusic;
     private Rigidbody rbd;
@@ -16,22 +15,11 @@ public class LineController : MonoBehaviour
     {
         backgroundMusic = GetComponent<AudioSource>();
         rbd = GetComponent<Rigidbody>();
-        growth.Add(transform);
-    }
-
-    void LineGrow()
-    {
-        Transform p = growth[growth.Count-1];
-        Instantiate(shadowPrefab, p.position, p.rotation);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if (growth.Contains(transform) == false) {
-        //    growth.Add(transform);
-        //    LineGrow();
-        //}
         if (Input.GetKeyDown(KeyCode.Space))
         {
             // start moving and play music
@@ -52,13 +40,15 @@ public class LineController : MonoBehaviour
             }
 
         }
+
+        if (dir != Vector3.zero) { Instantiate(shadowPrefab, transform.position, transform.rotation); }
         float movement = speed * Time.deltaTime;
         transform.Translate(dir * movement);
     }
     // TODO: add colliders on furniture
     void OnCollisionEnter(Collision collision)
     {
-        rbd.velocity = Vector3.zero;
+        dir = Vector3.zero;
         // display menu
     }
 }
